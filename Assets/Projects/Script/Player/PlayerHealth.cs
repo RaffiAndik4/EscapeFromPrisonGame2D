27d352cbf7fb5f6d84ec20f1dd570a4848a28bfa
Manut <email.raffi.andika19@gmail.com>
2025-12-28 22:31:04 +0700

    update

---
+++ Assets/Projects/Script/Player/PlayerHealth.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 3;
+    private int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        // Load scene awal (index 0)
+        SceneManager.LoadScene(0);
+    }
+}
